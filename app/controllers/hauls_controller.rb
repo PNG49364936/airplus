a5f1dce4ed42b270f4a9fc6c492f86ef5ba2736f
Pura Montero <puramontero@Puras-iMac.local>
2023-10-22 18:30:26 +0200

    commit 3

--- app/controllers/hauls_controller.rb
+++ app/controllers/hauls_controller.rb
@@ -1,2 +1,46 @@
 class HaulsController < ApplicationController
+
+    before_action   :set_haul, only:[:show, :edit, :upgrade, :destroy]
+
+def index 
+    @hauls = Hauls.all
+end
+
+def show 
+end
+
+def new 
+    @haul = haul.new
+end 
+
+def create
+    @haul = Haul.new(params_haul)
+    if @haul.save
+        redirect_to @haul, notice: "Haul succesfully created"
+        else
+        render :new
+    end
+end
+
+def edit
+end
+
+def upgrade
+end
+
+def destroy
+    @haul.destroy
+    redirect_to @haul_url
+
+end
+
+private
+    def set_haul
+       @haul = Haul.find(params[:id]) 
+    end
+
+    def params_haul
+        params.require(:haul).permit(:name)
+    end
+
 end
