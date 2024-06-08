8a60ecc8b4fa05df348a7f1da11fc5df78050bc9
Pura Montero <puramontero@Puras-iMac.local>
2024-06-08 15:29:03 +0200

    09jun

---
+++ app/javascript/controllers/aircrafts_flights_controller.js
@@ -0,0 +1,51 @@
+
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  static targets = ["aircraftSelect"];
+
+  connect() {
+    
+    this.haulSelect = document.querySelector('#flight_haul_id');
+    console.log ('aircrafts_flights_controller est connecté')
+    if (this.haulSelect) {
+        this.haulSelect.addEventListener('change', this.updateAircrafts.bind(this));
+      }
+  }
+
+  updateAircrafts(event) {
+    const selectedHaulId = event.target.value;
+    console.log("event target value",selectedHaulId)
+    if (selectedHaulId) {
+        fetch(`/flights/available_aircrafts?haul_id=${selectedHaulId}`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => {
+            console.log('date',data)
+          const aircraftMessage = document.getElementById('aircraft-message');
+          if (data.length === 0) {
+            // Affiche le message personnalisé dans l'élément HTML
+            aircraftMessage.textContent = "Aucun appareil disponible pour le haul sélectionné.";
+            aircraftMessage.classList.remove('d-none');
+          } else {
+            aircraftMessage.classList.add('d-none'); // Cache le message si data.length > 0
+          }
+          console.log('aircraftselect')
+          const aircraftSelect = this.aircraftSelectTarget;
+          console.log("aircraftSelect", aircraftSelect);
+          aircraftSelect.innerHTML = '';
+
+          data.forEach(aircraft => {
+            const option = new Option(aircraft.acft, aircraft.id);
+            console.log("test14", option);
+            aircraftSelect.appendChild(option);
+          });
+        })
+        .catch(error => console.error("Error loading aircrafts:", error));
+    }
+  }
+}
